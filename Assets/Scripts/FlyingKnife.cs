81980911670036771b8415947067e20b4a8788d6
kksumm123 <kksumm123@gmail.com>
2021-10-26 04:09:09 +0900

    1. GameManager.cs 아무 키 누르면 칼날 발싸 ! 뿡뿡뿡 ! 2. FlyingKnife.cs Start() destroy 5초

--- Assets/Scripts/FlyingKnife.cs
+++ Assets/Scripts/FlyingKnife.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class FlyingKnife : MonoBehaviour
 {
-    [SerializeField] float speed = 20f;
+    [SerializeField] float speed = 25f;
     Vector2 moveDirection = new Vector2(0, 1);
+    private void Start()
+    {
+        Destroy(gameObject, 5);
+    }
     void Update()
     {
         transform.Translate(speed * Time.deltaTime * moveDirection);
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private const string bulletKnifeGoString = "BulletKnife";
+    GameObject bulletKnifeGo;
     [SerializeField] int knifeCount = 10;
     void Start()
     {
         KnifeCountUI.Instance.SetKnifeIcon(knifeCount);
+        bulletKnifeGo = (GameObject)Resources.Load(bulletKnifeGoString);
     }
 
     void Update()
     {
-        
+        if (Input.anyKeyDown)
+            Instantiate(bulletKnifeGo);
     }
 }
