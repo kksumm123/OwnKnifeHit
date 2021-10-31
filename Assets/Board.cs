f292cf4c8f905df2532bce0ccdde936e7f2a9179
kksumm123 <kksumm123@gmail.com>
2021-10-31 18:44:06 +0900

    1. 박힌칼 추가 2. 나무에 칼이 박히면 박힌 칼 생성 3. 박힌 칼 손잡이에 칼이 맞으면 튕겨나가도록

--- Assets/Board.cs
+++ Assets/Board.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    const string stuckedKnifeString = "StuckedKnife";
+    GameObject stuckedKnifeGo;
     void Start()
     {
-        
+        stuckedKnifeGo = (GameObject)Resources.Load(stuckedKnifeString);
     }
 
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -21,5 +23,10 @@ public class Board : MonoBehaviour
 
         // 나이프가 충돌
         print("아얏 아포 힝");
+        // 날라가는 칼 부수기
+        Destroy(collision.collider.gameObject);
+        // 아래위치에 박힌칼 생성, 부모는 보드
+        var newKnife = Instantiate(stuckedKnifeGo);
+        newKnife.transform.parent = transform;
     }
 }
--- Assets/Scripts/FlyingKnife.cs
+++ Assets/Scripts/FlyingKnife.cs
@@ -6,12 +6,31 @@ public class FlyingKnife : MonoBehaviour
 {
     [SerializeField] float speed = 25f;
     Vector2 moveDirection = new Vector2(0, 1);
+    Rigidbody2D rigid;
     private void Start()
     {
         Destroy(gameObject, 5);
+        rigid = GetComponent<Rigidbody2D>();
+        isThrowing = true;
     }
+    bool isThrowing = false;
     void Update()
     {
-        transform.Translate(speed * Time.deltaTime * moveDirection);
+        if (isThrowing)
+            transform.Translate(speed * Time.deltaTime * moveDirection);
+    }
+    /*
+    1. 나이프끼리는 충돌안되게
+    2. 던지는 딜레이
+    3. 던지는 나이프, 박힌 나이프 태그 구별
+     */
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("StuckedKnife"))
+        {
+            isThrowing = false;
+            rigid.AddForce(Vector2.down * 300f, ForceMode2D.Force);
+        }
     }
 }
