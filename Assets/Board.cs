7c6c570949dee9ef451bad790ef3beda90baa664
kksumm123 <kksumm123@gmail.com>
2021-10-31 18:11:39 +0900

    1. Dotween 설치 2. RotateZ.cs 추가 및 작성완료, 보드에 추가 3. Board.cs 추가 및 작성 중

---
+++ Assets/Board.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Knife") == false)
+            return;
+
+        // 나이프가 충돌
+    }
+}
---
+++ Assets/RotateZ.cs
@@ -0,0 +1,29 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateZ : MonoBehaviour
+{
+    [SerializeField] float minZ = 1000f;
+    [SerializeField] float maxZ = 1800f;
+    [SerializeField] float duration = 5f;
+    bool isEnd = false;
+    [SerializeField] Ease ease = Ease.InOutCubic;
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            isEnd = false;
+            var rotateZ = transform.eulerAngles.z;
+            var endZ = Random.Range(minZ, maxZ);
+            DOTween.To(() => rotateZ, z => transform.eulerAngles = new Vector3(0, 0, z), endZ, duration)
+                   .OnComplete(() => isEnd = true)
+                   .SetLink(gameObject)
+                   .SetEase(ease);
+
+            while (isEnd == false)
+                yield return null;
+        }
+    }
+}
