0f182bc4127d18b7f4c368b065c0d2f988bb5322
kksumm123 <kksumm123@gmail.com>
2021-10-31 18:57:38 +0900

    칼 던지기 전에 보여진 상태에서 가만히 있도록

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,11 +12,34 @@ public class GameManager : MonoBehaviour
     {
         KnifeCountUI.Instance.SetKnifeIcon(knifeCount);
         bulletKnifeGo = (GameObject)Resources.Load(bulletKnifeGoString);
+        CreateKnife();
     }
 
     void Update()
     {
-        if (Input.anyKeyDown)
-            Instantiate(bulletKnifeGo);
+        if (Input.anyKeyDown && isThrowable == true)
+            StartCoroutine(ClickedCo());
+    }
+
+    bool isThrowable = false;
+    IEnumerator ClickedCo()
+    {
+        isThrowable = false;
+        ThrowKnife();
+        yield return new WaitForSeconds(0.1f);
+        CreateKnife();
+    }
+
+    GameObject currentKnife;
+    void CreateKnife()
+    {
+        currentKnife = Instantiate(bulletKnifeGo);
+        currentKnife.GetComponent<FlyingKnife>().enabled = false;
+        isThrowable = true;
+    }
+    void ThrowKnife()
+    {
+        currentKnife.GetComponent<FlyingKnife>().enabled = true;
     }
 }
+
