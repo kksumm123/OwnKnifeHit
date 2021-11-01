740f93aadae7c9e1845aefadb064f66af6bfbd46
kksumm123 <kksumm123@gmail.com>
2021-11-02 07:55:16 +0900

    appleScore PlayerPref 이용하여 저장

--- Assets/Scripts/AppleScoreUI.cs
+++ Assets/Scripts/AppleScoreUI.cs
@@ -5,12 +5,19 @@ using UnityEngine.UI;
 
 public class AppleScoreUI : BaseUI<AppleScoreUI>
 {
+    private const string appleScoreKey = "AppleScore";
     int appleScore = 0;
     Text appleScoreText;
     void Start()
     {
+        appleScore = PlayerPrefs.GetInt(appleScoreKey, 0);
+
         appleScoreText = transform.Find("Value").GetComponent<Text>();
         appleScoreText.text = appleScore.ToString();
+    }
+    private void OnDestroy()
+    {
+        PlayerPrefs.SetInt(appleScoreKey, appleScore);
     }
 
     internal void AddApplePoint(int addValue)
